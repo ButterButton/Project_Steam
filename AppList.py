169650da0277a95d7412b0e9aacb0608e867a78a
ButterButton <tim126359@gmail.com>
2019-03-25 17:59:53 +0800

    Get All Hot Sell

--- AppList.py
+++ AppList.py
@@ -5,70 +5,77 @@ from bs4 import BeautifulSoup
 import datetime
 import json
 from flask_cors import CORS
-import pytz
-import time
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/Get")
 def index():
-    res = requests.get("https://store.steampowered.com/search/?filter=topsellers&os=win&ignore_preferences=1&cc=TW")
-    soup = BeautifulSoup(res.text, 'html.parser')
-    tw = pytz.timezone("Asia/Taipei")
-    dt = datetime.datetime.now(tw)
-    GameList = {
-        "type" : "topseller",
-	    "datetime" : dt.strftime("%Y-%m-%d_%H:%M"),
-	    "total_app" : 0,
-	    "startrank" : 0,
-        "endtrank" : 0,
-        "applications" : []
-    }
-    HTMLAppInfo = soup.find_all("a", class_="search_result_row")
-    HTMLAppPic = soup.find_all("div", class_="search_capsule")
-    HTMLAppName = soup.find_all("span", class_="title")
-    HTMLAppEval = soup.select(".search_reviewscore")
-    HTMLAppDis = soup.select(".search_discount")
-    HTMLAppPrice = soup.find_all("div", class_="search_price")
-
-    i = 0
-    pages = 1
+    RankOrder = 1
+    PageNumber = 1
     tempapplist = []
+    GameList = {
+            "type" : "topseller",
+            "datetime" : datetime.datetime.now().strftime("%Y-%m-%d_%H:%M"),
+            "total_app" : 0,
+            "startrank" : 0,
+            "endtrank" : 0,
+            "applications" : []
+    }
 
-    for item in HTMLAppInfo:
-        application = {}
-        link = item["href"]
-        applink = link.split("/")
-        
-        application["steam_link"] = link
-        application["app_id"] = applink[4]
-        application["app_type"] = applink[3]
-        application["small_pic"] = GetAppSmPic(HTMLAppPic)[i]
-        application["header_pic"] = GetHeaderPic(application["small_pic"].split("/"), applink)
-        application["app_name"] = GetAppName(HTMLAppName)[i]
-        application["evaluation"] = GetAppEval(HTMLAppEval)[i]
-        application["discount"] = GetAppDis(HTMLAppDis)[i]
-        application["original_price"] = GetAppOrgPrice(HTMLAppPrice)[i]
-        application["discount_price"] = GetAppDisPrice(HTMLAppPrice)[i]
+    while(True):     
+        # res = requests.get("https://store.steampowered.com/search/?filter=topsellers&os=win&ignore_preferences=1&cc=TW")
+        Url = "https://store.steampowered.com/search/?ignore_preferences=1&filter=topsellers&os=win&cc=TW&page=" + str(PageNumber)
+        print(Url)
+        res = requests.get(Url)
+        soup = BeautifulSoup(res.text, 'html.parser')
+        HTMLAppInfo = soup.find_all("a", class_="search_result_row")
+        HTMLAppPic = soup.find_all("div", class_="search_capsule")
+        HTMLAppName = soup.find_all("span", class_="title")
+        HTMLAppEval = soup.select(".search_reviewscore")
+        HTMLAppDis = soup.select(".search_discount")
+        HTMLAppPrice = soup.find_all("div", class_="search_price")
+        HTMLEndPage = soup.find_all("p",text="No results were returned for that query.")
 
-        i = i + 1
-        
-        if(i % 8 == 0):
-            pages = pages + 1
+        print(HTMLEndPage)
+        pages = 1
 
-        application["page"] = pages
-        application["rank"] = i
-        tempapplist.append(application)
+
+        if(HTMLEndPage == []):
+            i = 0
+            PageNumber = PageNumber + 1
+            print("Im Here")
+            for item in HTMLAppInfo:
+                application = {}
+                link = item["href"]
+                applink = link.split("/")              
+                application["steam_link"] = link
+                application["app_id"] = applink[4]
+                application["app_type"] = applink[3]
+                application["small_pic"] = GetAppSmPic(HTMLAppPic)[i]
+                application["header_pic"] = GetHeaderPic(application["small_pic"].split("/"), applink)
+                application["app_name"] = GetAppName(HTMLAppName)[i]
+                application["evaluation"] = GetAppEval(HTMLAppEval)[i]
+                application["discount"] = GetAppDis(HTMLAppDis)[i]
+                application["original_price"] = GetAppOrgPrice(HTMLAppPrice)[i]
+                application["discount_price"] = GetAppDisPrice(HTMLAppPrice)[i]
+
+                i = i + 1
+                RankOrder = RankOrder + 1
+
+                if(i % 8 == 0):
+                    pages = pages + 1
+
+                application["page"] = pages
+                application["rank"] = RankOrder
+
+                tempapplist.append(application)
+        else:
+            break
 
     GameList["applications"] = tempapplist
-    GameList["total_app"] = i
+    GameList["total_app"] = RankOrder
     return Response(json.dumps(GameList, ensure_ascii=False, indent=4), mimetype='text/json')
-
-@app.route("/Get/<int:Rank>")
-def GetTop(Rank):
-    print("Hellow")
-    return Rank.text
 
 def GetAppSmPic(HtmlAppPic):
     PicList = []
